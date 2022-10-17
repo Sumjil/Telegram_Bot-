dfb7fa3ec04efbaa8f83f549099ebab60f87206a
Yulia Kashinskaya <yulia@MacBook-Air-Yulia.local>
2022-10-17 12:52:32 +0300

    Users can create a to-do list

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,33 +2,43 @@ import telebot
 from telebot import types
 import configure
 
+# in the future will be
+# def edit_list
+# when a task is done bot sends a sticker or cheer-up text
+
 bot = telebot.TeleBot(configure.config['token'])
+to_do_list =[]
 
-
-@bot.message_handler(commands=['start']) 
+@bot.message_handler(commands=['start'])
 def create_list(message):
 
     rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    rmk.add(types.KeyboardButton("Create a note"), types.KeyboardButton("Sticker"))
+    rmk.add(types.KeyboardButton("Create a to-do list"), types.KeyboardButton("Give me just a Sticker"))
     msg = bot.send_message(message.chat.id, "What do you want?", reply_markup= rmk)
     bot.register_next_step_handler(msg, user_answer)
 
-
-
-
 def user_answer(message):
 
-    if message.text == "Create a note":
-        msg = bot.send_message(message.chat.id, "Write want you want....")
-        bot.register_next_step_handler(msg,user_to_do_list)
-    elif message.text == "Sticker":
+    if message.text == "Create a to-do list":
+        bot.send_message(message.from_user.id,
+                         "Add your tasks and when you finish send me a dot .")
+        bot.register_next_step_handler(message, get_task)
+    elif message.text == "Give me just a Sticker":
         bot.send_sticker(message.chat.id,
                          sticker='CAACAgIAAxkBAAEGFtJjSYwDJZN1Ht7xFNuABAo9OSZBdwAC6RsAAoV_EEnRch0GfnFH5yoE')
     else:
-        bot.send_message(message.chat.id, "I can't understand you")
+        bot.send_message(message.from_user.id, "I can't understand you :(")
 
-def user_to_do_list(message):
-    bot.send_message(message.chat.id,f"Your note: {message.text}")
 
+def get_task(message):
+    global to_do_list
+    text_from_user = message.text
+    if text_from_user != '.':
+        bot.send_message(message.from_user.id, 'Added')
+        to_do_list.append(text_from_user)
+        bot.register_next_step_handler(message, get_task)
+    else:
+        st = "\n".join(to_do_list)
+        bot.send_message(message.from_user.id, f"Your to-do list:\n{st}")
 
 bot.polling(none_stop=True, interval = 0)
